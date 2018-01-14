6c7084a624e349447feda78963d82862a8e5fa77
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-01-13 18:01:22 -0800

    Added Filter for check feature

--- app.js
+++ app.js
@@ -1,6 +1,41 @@
 const form = document.getElementById('registrar');
 const input = form.querySelector('input');
+
+const mainDiv = document.querySelector('.main');
 const ul = document.getElementById('invitedList');
+
+const div = document.createElement('div');
+const filterLabel = document.createElement('label');
+const filterCheckBox = document.createElement('input');
+
+filterLabel.textContent = "Hide those who haven't responded";
+filterCheckBox.type = 'checkbox';
+div.appendChild(filterLabel);
+div.appendChild(filterCheckBox);
+mainDiv.insertBefore(div, ul);
+
+// Check for invitees not confirmed
+filterCheckBox.addEventListener('change', (e) => {
+    const isChecked = e.target.checked;
+    const lis = ul.children;
+    if (isChecked) {
+        for (let i = 0; i < lis.length; i += 1) {
+            let li = lis[i];
+            if (li.className === 'responded') {
+                li.style.display = '';
+            } else {
+                li.style.display = 'none';
+            }
+        }
+    } else {
+        for (let i = 0; i < lis.length; i += 1) {
+            let li = lis[i];
+            li.style.display = '';
+        }
+    }
+});
+
+
 
 // Create List Item when form is submitted
 function createLi(text) {
