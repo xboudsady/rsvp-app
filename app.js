8cb71e5334d6bdade9e8223ff15eb0a9a739d292
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-01-13 18:20:15 -0800

    Added DOMContentLoaded event

--- app.js
+++ app.js
@@ -1,109 +1,111 @@
-const form = document.getElementById('registrar');
-const input = form.querySelector('input');
+document.addEventListener('DOMContentLoaded', () => {
 
-const mainDiv = document.querySelector('.main');
-const ul = document.getElementById('invitedList');
+    const form = document.getElementById('registrar');
+    const input = form.querySelector('input');
 
-const div = document.createElement('div');
-const filterLabel = document.createElement('label');
-const filterCheckBox = document.createElement('input');
+    const mainDiv = document.querySelector('.main');
+    const ul = document.getElementById('invitedList');
 
-filterLabel.textContent = "Hide those who haven't responded";
-filterCheckBox.type = 'checkbox';
-div.appendChild(filterLabel);
-div.appendChild(filterCheckBox);
-mainDiv.insertBefore(div, ul);
+    const div = document.createElement('div');
+    const filterLabel = document.createElement('label');
+    const filterCheckBox = document.createElement('input');
 
-// Check for invitees not confirmed
-filterCheckBox.addEventListener('change', (e) => {
-    const isChecked = e.target.checked;
-    const lis = ul.children;
-    if (isChecked) {
-        for (let i = 0; i < lis.length; i += 1) {
-            let li = lis[i];
-            if (li.className === 'responded') {
+    filterLabel.textContent = "Hide those who haven't responded";
+    filterCheckBox.type = 'checkbox';
+    div.appendChild(filterLabel);
+    div.appendChild(filterCheckBox);
+    mainDiv.insertBefore(div, ul);
+
+    // Check for invitees not confirmed
+    filterCheckBox.addEventListener('change', (e) => {
+        const isChecked = e.target.checked;
+        const lis = ul.children;
+        if (isChecked) {
+            for (let i = 0; i < lis.length; i += 1) {
+                let li = lis[i];
+                if (li.className === 'responded') {
+                    li.style.display = '';
+                } else {
+                    li.style.display = 'none';
+                }
+            }
+        } else {
+            for (let i = 0; i < lis.length; i += 1) {
+                let li = lis[i];
                 li.style.display = '';
-            } else {
-                li.style.display = 'none';
             }
         }
-    } else {
-        for (let i = 0; i < lis.length; i += 1) {
-            let li = lis[i];
-            li.style.display = '';
-        }
+    });
+
+    // Create List Item when form is submitted
+    function createLi(text) {
+        const li = document.createElement('li');
+        const span = document.createElement('span');
+        span.textContent = text;
+        li.appendChild(span);
+        const label = document.createElement('label');
+        label.textContent = 'Confirmed';
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        label.appendChild(checkbox);
+        li.appendChild(label);
+        
+        const editButton = document.createElement('button');
+        editButton.textContent = 'edit';
+        li.appendChild(editButton);
+
+        const removeButton = document.createElement('button');
+        removeButton.textContent = 'remove';
+        li.appendChild(removeButton);
+        return li;
     }
-});
 
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const text = input.value;
+        input.value = "";
+        const li = createLi(text);
+        ul.appendChild(li);
+    });
 
+    // Checked Boxed Event
+    ul.addEventListener('change', (e) => {
+        const checkbox = e.target;
+        const checked = checkbox.checked;
+        const listItem = checkbox.parentNode.parentNode;
 
-// Create List Item when form is submitted
-function createLi(text) {
-    const li = document.createElement('li');
-    const span = document.createElement('span');
-    span.textContent = text;
-    li.appendChild(span);
-    const label = document.createElement('label');
-    label.textContent = 'Confirmed';
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    label.appendChild(checkbox);
-    li.appendChild(label);
-    
-    const editButton = document.createElement('button');
-    editButton.textContent = 'edit';
-    li.appendChild(editButton);
-
-    const removeButton = document.createElement('button');
-    removeButton.textContent = 'remove';
-    li.appendChild(removeButton);
-    return li;
-}
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const text = input.value;
-    input.value = "";
-    const li = createLi(text);
-    ul.appendChild(li);
-});
-
-// Checked Boxed Event
-ul.addEventListener('change', (e) => {
-    const checkbox = e.target;
-    const checked = checkbox.checked;
-    const listItem = checkbox.parentNode.parentNode;
-
-    if (checked) {
-        listItem.className = 'responded';
-    } else {
-        listItem.className = '';
-    }
-});
-
-// Remove Button Event
-ul.addEventListener('click', (e) => {
-    if (e.target.tagName === 'BUTTON') {
-        const button = e.target;
-        const li = button.parentNode;
-        const ul = li.parentNode;
-        if (button.textContent === 'remove') {
-            ul.removeChild(li);
-        } else if (button.textContent === 'edit') {
-            const span = li.firstElementChild;
-            const input = document.createElement('input');
-            input.type = 'text';
-            input.value = span.textContent;
-            li.insertBefore(input, span);
-            li.removeChild(span);
-            button.textContent = 'save';
-        } else if (button.textContent === 'save') {
-            const input = li.firstElementChild;
-            const span = document.createElement('span');
-            span.textContent = input.value;
-            li.insertBefore(span, input);
-            li.removeChild(input);
-            button.textContent = 'edit';
+        if (checked) {
+            listItem.className = 'responded';
+        } else {
+            listItem.className = '';
         }
-    } 
+    });
+
+    // Remove Button Event
+    ul.addEventListener('click', (e) => {
+        if (e.target.tagName === 'BUTTON') {
+            const button = e.target;
+            const li = button.parentNode;
+            const ul = li.parentNode;
+            if (button.textContent === 'remove') {
+                ul.removeChild(li);
+            } else if (button.textContent === 'edit') {
+                const span = li.firstElementChild;
+                const input = document.createElement('input');
+                input.type = 'text';
+                input.value = span.textContent;
+                li.insertBefore(input, span);
+                li.removeChild(span);
+                button.textContent = 'save';
+            } else if (button.textContent === 'save') {
+                const input = li.firstElementChild;
+                const span = document.createElement('span');
+                span.textContent = input.value;
+                li.insertBefore(span, input);
+                li.removeChild(input);
+                button.textContent = 'edit';
+            }
+        } 
+    });
+
 });
